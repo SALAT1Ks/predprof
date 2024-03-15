b1e51b9959bd4a90d00129171dd0290da446fddb
SALAT1Ks <123331549+SALAT1Ks@users.noreply.github.com>
2024-03-16 01:04:54 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -25,18 +25,23 @@ def linear_search(arr, target):
     Returns:
         None
     """
+    c = 0
     for i in range(len(arr)):
-        if arr[i][0] == target :
+        if arr[i][2] == target:
             name = arr[i][1].split(' ')
             print(f'Проект № {arr[i][0]} делал: {name[1][0]}. {name[0]} он(а) получил(а) оценку - {arr[i][4]}.')
+            c+=1
+    if c == 0:
+        print('Ничего не найдено.')
+
 
 def main():
     """
     Основная функция для обработки данных студентов из CSV-файла.
     """
     file_path = "C:/Users/oppoe/Downloads/students (1).csv"
-    data = read_csv_file(file_path)
-    print(data)
+    data = read_csv_file(file_path)[:-1]
+
     while True:
         id_project = input()
         if id_project == 'СТОП':
